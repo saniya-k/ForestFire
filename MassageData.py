2512ddd6e84ac384d23e52bc6d048a2c40d09130
saniya <saniya965@gmail.com>
2020-03-03 09:48:29 -0500

    Adding code to get data from Australian SILO

---
+++ MassageData.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 14:54:25 2020
+
+@author: Saniya Khan
+"""
+import pandas as pd
+data= pd.read_csv("weather.csv")
+print(data)
+col_names=["Date (yyyymmdd)","Day","T.Max(oC)","Smx","T.Min(oC)","Smn","Rain (mm)","Srn","Evap(mm)","Sev", "Radn(MJ/m2)","Ssl" ,"VP (hPA)","Svp","RHmaxT(%)" ,"RHminT(%)" ,"Date2(ddmmyyyy)","Brigade"]
+print(data.row)
+df = pd.DataFrame(data.row.str.split(',',n=17).tolist(),
+                                   columns = col_names)
+print(df.head())
+df.to_csv("Weather_formatted.csv")
---
+++ getData_try.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 12:54:11 2020
+
+@author: Saniya Khan
+"""
+from __future__ import unicode_literals
+import urllib.request
+import urllib.parse
+import pandas as pd
+from io import StringIO
+from itertools import repeat
+api_url = 'https://www.longpaddock.qld.gov.au/cgi-bin/silo'
+#replace by actual geocodes
+#geocode=pd.DataFrame([['YANKALILLA','-35.494262', '138.362596'], 
+#              ['PORT WAKEFIELD','-34.185349', '138.155379']],columns=['Brigade','latitude','longitude'])
+list_of_weather=[] 
+weather=pd.DataFrame()
+def getGeocode():
+    geocode= pd.read_csv("coord.csv")
+    print(geocode.columns)
+    geocode.set_index('Brigade')
+    #print("dfdfdfD")
+    #print(geocode)
+    return geocode;
+def buildUrl(lat,long):
+    params = {
+    'format': 'standard',
+    'lat': str(lat),
+    'lon': str(long),
+    'start': '20090101',
+    'finish': '20171231',
+    'username': 'sk3862@drexel.edu',
+    'password': 'silo'
+    }
+    url = api_url + '/DataDrillDataset.php?' + urllib.parse.urlencode(params)
+    return url    
+def sendRequest(url):
+    with urllib.request.urlopen(url) as remote:
+        data = remote.read()
+        s=str(data,'utf-8')
+        data_formatted = StringIO(s) 
+        df=pd.read_csv(data_formatted)
+    return df
+def getData(lat,long) :
+
+    return weather
+
+#lat long index
+geocode=getGeocode()
+for i in range(len(geocode)):
+    print(i)
+    brigade=[geocode.loc[i,'Brigade']]
+    url=buildUrl(geocode.loc[i,'latitude'],  geocode.loc[i,'longitude']) #url for lat long
+    df=sendRequest(url) #ping the australian website
+    if  i==0: 
+        headr=df.iloc[31,:]
+        headr.reset_index(inplace=True, drop=True)
+        headr.replace('\s+', ',',regex=True,inplace=True) #separate out the header
+        headr[0]=headr[0]+",Brigade"
+        list_of_weather.append(headr) 
+    df=df[32:(len(df)-1)]#cleaning remove header, indexes
+    df.replace('\s+', ',',regex=True,inplace=True) #make csv space delimited to comma delimited
+    df=df.iloc[:,-1]#cleaning 
+    df.name=brigade[0]
+    formatted_data=df+","+brigade[0]
+    list_of_weather.append(formatted_data)#combine for different locations
+    weather=pd.concat(list_of_weather)
+#weather = [getData(x, y) for x, y in zip(geocode['latittude'], geocode['longitude'])]
+weather.to_csv('weather.csv',encoding='utf-8',header=False)#save to file """
